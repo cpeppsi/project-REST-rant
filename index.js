d6a9f87566677435c1fb5ed59d55fcee522a929e
cpeppsi <caepps28@gmail.com>
2023-04-30 08:52:52 -0400

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ app.get('*', (req, res) => {
   res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(process.env.PORT, console.log(`listening on port ${PORT}`))
+app.listen(process.env.PORT, console.log(`listening on port 3000`))
