278e9266921599259c716d4efafbc7220cf5cd24
cpeppsi <caepps28@gmail.com>
2023-05-18 21:44:48 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -29,6 +29,19 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+router.get('/:id', (req, res) => {
   const { places } = req.params
   res.render('places')
 })
